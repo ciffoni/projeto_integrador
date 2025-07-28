3b60c3c85a0f897aa1fcf71898960b5c43c629dc
jorge ciffoni <ciffoni@gmail.com>
2025-07-28 11:17:04 -0300

    banco

--- banco de dados.sql
+++ banco de dados.sql
@@ -1,1 +1,10 @@
 create database projeto;
+use projeto;
+create table usuario(
+codigo int primary key auto_increment,
+nome varchar(100),
+email varchar(100),
+senha varchar(255),
+cargo varchar(60)
+
+);
