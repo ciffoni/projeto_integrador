f8bdabf8772d83dd80d9837bf703cdcda81f9f10
jorge ciffoni <ciffoni@gmail.com>
2025-07-30 11:27:13 -0300

    banco

--- banco de dados.sql
+++ banco de dados.sql
@@ -1,10 +1,71 @@
 create database projeto;
 use projeto;
+create table cargo(
+codigocargo int primary key auto_increment,
+cargo varchar(100)
+);
 create table usuario(
-codigo int primary key auto_increment,
+codigo int primary key auto_increment ,
 nome varchar(100),
 email varchar(100),
 senha varchar(255),
-cargo varchar(60)
-
+cargo int,
+ativo tinyint,
+constraint fk_cargo foreign key (cargo) references cargo(codigocargo)
 );
+
+create table tipo(
+codigotipo int primary key auto_increment,
+descricao varchar(100)
+);
+
+create table produto(
+
+codigoProduto int primary key auto_increment,
+descricao varchar(100),
+valor decimal(5,2),
+quantidade int,
+foto varchar(100),
+nome varchar(100),
+tipo int,
+constraint fk_tipo foreign key (tipo) references tipo(codigotipo)
+);
+
+create table cliente(
+codigocliente int primary key auto_increment,
+nome varchar(60),
+fone varchar(15),
+email varchar(100),
+cnpj_cpf varchar(30),
+cep varchar(20),
+num_casa varchar(4),
+complemento varchar(100)
+);
+create table pedido(
+codigopedido int primary key auto_increment,
+data_pedido datetime,
+data_entrega datetime,
+codigousuario int,
+codigocliente int,
+formapagamento varchar(60),
+constraint fk_usuario foreign key (codigousuario) references usuario(codigo),
+constraint fk_cliente foreign key (codigocliente) references cliente(codigocliente)
+);
+
+create table pedido_itens(
+codigoitens int primary key auto_increment,
+codigopedido int,
+codigoproduto int,
+quantidade int,
+valor decimal(5,2),
+total decimal(5,2),
+constraint fk_pedido foreign key (codigopedido) references pedido(codigopedido),
+constraint fk_produto foreign key(codigoproduto) references produto(codigoproduto)
+);
+
+create table fluxocaixa(
+codigofluxo int primary key auto_increment,
+descricao varchar(100),
+tipo varchar(50),
+valor decimal(5,2)
+)
